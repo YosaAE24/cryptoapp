aacfacd80456ce0a96d880b4daecc5cd42d4221d
YosaAE24 <cebastin2424@gmail.com>
2024-08-13 18:33:49 +0700

    make app cryptoverse with-reat

---
+++ src/components/News.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import moment from 'moment';
+
+
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const News = (simplified) => {
+  return (
+    <div>
+      News
+    </div>
+  )
+}
+
+export default News
